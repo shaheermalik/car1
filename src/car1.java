ce81162c184990453e782d0e020599a24ed3bff1
shaheermalik <31680920+shaheermalik@users.noreply.github.com>
2017-12-05 15:02:24 -0500

    Initial commit

---
+++ src/car1.java
@@ -0,0 +1,45 @@
+public class car1 {
+
+
+    String color;
+    int horsepower;
+    String make;
+    double engineSize;
+
+    public car1() {
+        this.make = "Mclaren";
+        this.color = "purple";
+        this.horsepower =710;
+        this.engineSize = 4.0; }
+    public car1(String color, String make, int horsepower, double engineSize) {
+        this.color = color;
+        this.make = make;
+        this.horsepower = horsepower;
+        this.engineSize = engineSize; }
+
+    public String getMake() { return make; }
+
+    public void setMake(String newMake) {
+        this.make = newMake; }
+
+    public String getColor() { return color; }
+
+    public void SetColor(String newColor) {
+        this.color = newColor; }
+
+    public int getHorsepower() { return horsepower; }
+
+    public void setHorsepower(int newHorsepower) {
+        this.horsepower = newHorsepower; }
+
+    public double getEngineSize() { return engineSize; }
+
+    public void setEngineSize(double newEngineSize) {
+        this.engineSize = newEngineSize; }
+
+    public String toString() {
+        String output = (this.color + " " + this.make + ", " + this.engineSize);
+        return output;}
+
+}
+
